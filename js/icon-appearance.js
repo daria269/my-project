a3c17f84e9cca3ef713c9ffc81e51806fae236e8
Daria <dasha_kravchenko_00@mail.ru>
2020-09-02 00:08:15 +0300

    changed opacity appearance to lazyimg

--- js/icon-appearance.js
+++ js/icon-appearance.js
@@ -1,23 +1,48 @@
-const icons = document.querySelectorAll('.tour-conditions__icon');
+// const icons = document.querySelectorAll('.tour-conditions__icon');
+
+// let options = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 1
+// }
+
+// const appear = function(entries, observer) {
+//   entries.forEach(entry => {
+//       let target = entry.target;
+//       if (entry.isIntersecting) {
+//         target.style.opacity = 1;
+//         observer.unobserve(target);
+//       }
+//   })
+// };
+
+// const observer = new IntersectionObserver(appear, options);
+
+// icons.forEach(icon => {
+//   observer.observe(icon);
+// });
+
+const images = document.querySelectorAll(`.lazy_img`);
 
 let options = {
   root: null,
-  rootMargin: '0px',
+  rootMargin: `0px`,
   threshold: 1
-}
+};
 
-const appear = function(entries, observer) {
-  entries.forEach(entry => {
-      let target = entry.target;
-      if (entry.isIntersecting) {
-        target.style.opacity = 1;
-        observer.unobserve(target);
-      }
-  })
+const appear = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const lazyImage = entry.target;
+      lazyImage.src = lazyImage.dataset.src;
+      lazyImage.classList.remove(`lazy_img`);
+      observer.unobserve(lazyImage);
+    }
+  });
 };
 
 const observer = new IntersectionObserver(appear, options);
 
-icons.forEach(icon => {
-  observer.observe(icon);
+images.forEach((image) => {
+  observer.observe(image);
 });
