522615aa41377edee63f2dd3e7a1bfbcdad31b91
Daria <dasha_kravchenko_00@mail.ru>
2020-09-02 13:50:29 +0300

    deleted extra lines

--- js/icon-appearance.js
+++ js/icon-appearance.js
@@ -1,27 +1,3 @@
-// const icons = document.querySelectorAll('.tour-conditions__icon');
-
-// let options = {
-//   root: null,
-//   rootMargin: '0px',
-//   threshold: 1
-// }
-
-// const appear = function(entries, observer) {
-//   entries.forEach(entry => {
-//       let target = entry.target;
-//       if (entry.isIntersecting) {
-//         target.style.opacity = 1;
-//         observer.unobserve(target);
-//       }
-//   })
-// };
-
-// const observer = new IntersectionObserver(appear, options);
-
-// icons.forEach(icon => {
-//   observer.observe(icon);
-// });
-
 const images = document.querySelectorAll(`.lazy_img`);
 
 let options = {
